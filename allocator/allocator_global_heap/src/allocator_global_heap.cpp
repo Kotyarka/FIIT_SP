33cfc526e596045a863eb6743489c5612f995395
Костян <varenik.kotyarka@gmail.com>
2026-04-16 00:09:01 +0300

    global heap done

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -2,44 +2,65 @@
 #include "../include/allocator_global_heap.h"
 
 allocator_global_heap::allocator_global_heap()
+    :   smart_mem_resource()
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap()", "your code should be here...");
 }
 
 [[nodiscard]] void *allocator_global_heap::do_allocate_sm(
     size_t size)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_global_heap::do_allocate_sm(size_t)", "your code should be here...");
+    std::lock_guard<std::mutex> lock(mutex);
+    if (size == 0) {
+        return nullptr;
+    }
+
+    void* ptr = ::operator new(size, std::nothrow);
+
+    if (ptr == nullptr) {
+        throw std::bad_alloc();
+    }
+
+    return ptr;
+
+
 }
 
 void allocator_global_heap::do_deallocate_sm(
     void *at)
 {
-    throw not_implemented("void allocator_global_heap::do_deallocate_sm(void *)", "your code should be here...");
+    std::lock_guard<std::mutex> lock(mutex);
+    ::operator delete(at);
 }
 
 allocator_global_heap::~allocator_global_heap()
 {
 }
 
-allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)
+allocator_global_heap::allocator_global_heap(const allocator_global_heap &other) : smart_mem_resource(other)
 {
-    throw not_implemented("allocator_global_heap::allocator_global_heap(const allocator_global_heap &other)", "your code should be here...");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(const allocator_global_heap &other)
 {
+    return *this;
 }
 
 bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept
 {
-    throw not_implemented("bool allocator_global_heap::do_is_equal(const std::pmr::memory_resource &other) const noexcept", "your code should be here...");
+    std::lock_guard<std::mutex> lock(mutex);
+    return this == &other;
 }
 
 allocator_global_heap::allocator_global_heap(allocator_global_heap &&other) noexcept
+    : smart_mem_resource(std::move(other))
 {
 }
 
 allocator_global_heap &allocator_global_heap::operator=(allocator_global_heap &&other) noexcept
 {
+    if (this != &other) {
+        std::lock_guard<std::mutex> lock(mutex);
+        smart_mem_resource::operator=(std::move(other));
+    }
+    return *this;
 }
